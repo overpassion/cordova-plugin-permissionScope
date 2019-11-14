f4966c0a7ba47f936cd5e8b81683c478848a043e
yhshin <overpassion@gmail.com>
2019-11-14 15:29:24 +0900

    apply SWIFT5

--- src/www/permissionScope.js
+++ src/www/permissionScope.js
@@ -1,34 +1,44 @@
-import exec from 'cordova/exec';
+cordova.define("cordova-plugin-permissionScope.PermissionScope", function(require, exports, module) {
 
-exports.init = (config, success, error) => {
-  exec(success, error, 'PermissionScope',  'initialize', [ config ]);
+(function () {
+
+var exec = require('cordova/exec');
+
+exports.init = function(config, success, error) {
+    exec(success, error, 'PermissionScope',  'initialize', [ config ]);
 };
 
 const types = [
-  'Notifications',
-  'LocationInUse',
-  'LocationAlways',
-  'Contacts',
-  'Events',
-  'Microphone',
-  'Camera',
-  'Photos',
-  'Reminders',
-  'Bluetooth',
-  'Motion'
-];
+               'Notifications',
+               'LocationInUse',
+               'LocationAlways',
+               'Contacts',
+               'Events',
+               'Microphone',
+               'Camera',
+               'Photos',
+               'Reminders',
+               'Bluetooth',
+               'Motion',
+               'Speech'
+               ];
 
-types.forEach((type) => {
-  const addPermissionMethod = `add${type}Permission`;
-  const requestPermissionMethod = `request${type}Permission`;
-  exports[addPermissionMethod] = (message, success, error) => {
-    exec(success, error, 'PermissionScope', 'addPermission', [ type, message ] );
-  };
-  exports[requestPermissionMethod] = (success, error) => {
-    exec(success, error, 'PermissionScope', 'requestPermission', [ type ] );
-  };
-})
+types.forEach(function(type) {
+              const addPermissionMethod = `add${type}Permission`;
+              const requestPermissionMethod = `request${type}Permission`;
+              exports[addPermissionMethod] = function(message, success, error) {
+              exec(success, error, 'PermissionScope', 'addPermission', [ type, message ] );
+              };
+              exports[requestPermissionMethod] = function(success, error) {
+              exec(success, error, 'PermissionScope', 'requestPermission', [ type ] );
+              };
+              })
 
-exports.show = (success, error) => {
-  exec(success, error, 'PermissionScope',  'show');
+exports.show = function(success, error) {
+    exec(success, error, 'PermissionScope',  'show');
 };
+
+
+ })();
+               
+});
